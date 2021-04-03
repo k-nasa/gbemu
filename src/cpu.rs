07861cd0492c371fe4484cc05a9cf542b4443762
k-nasa <htilcs1115@gmail.com>
2021-04-03 12:38:33 +0900

    implement all opcode mattern

--- src/cpu.rs
+++ src/cpu.rs
@@ -171,14 +171,307 @@ impl Cpu {
                 let operands = self.fetch_operands(1);
                 self.ldn_u8(TargetRegister::B, operands)
             }
-            0x07 => {
-                // RLCA, 0, 1
-                self.rlca();
+            0x07 => self.rlca(), // RLCA, 0, 1
+            0x08 => {
+                // LD (nn), SP
+                // 0x8, "LD (nn),SP", 2, 5, func(cpu *CPU, operands []byte) { cpu.ldnn_sp(operands) }},
             }
-            0x08 => {}
-            0x09 => {}
-            0x0A => {}
-            _ => bail!("not implemented opcode {:X}", opcode),
+            0x09 => {
+                // ADD HL, BC
+                // 0x9, "ADD HL,BC", 0, 2, func(cpu *CPU, operands []byte) { cpu.addhl_rr(&cpu.Regs.B, &cpu.Regs.C) }},
+            }
+            0x0A => {
+                // LD A, (BC)
+                // 0xA, "LD A,(BC)", 0, 2, func(cpu *CPU, operands []byte) { cpu.ldr_rr(cpu.Regs.B, cpu.Regs.C, &cpu.Regs.A) }},
+            }
+            0xB => {
+                // DEC BC
+                // 0xB, "DEC BC", 0, 2, func(cpu *CPU, operands []byte) { cpu.dec_nn(&cpu.Regs.B, &cpu.Regs.C) }},
+            }
+            0xC => {
+                // INC C
+                // 0xC, "INC C", 0, 1, func(cpu *CPU, operands []byte) { cpu.inc_n(&cpu.Regs.C) }},
+            }
+            0xD => {
+                // 0xD, "DEC C", 0, 1, func(cpu *CPU, operands []byte) { cpu.dec_n(&cpu.Regs.C) }},
+            }
+            0xE => {
+                // 0xE, "LD C,n", 1, 2, func(cpu *CPU, operands []byte) { cpu.ldnn_n(&cpu.Regs.C, operands) }},
+            }
+            0xF => {
+                // 0xF, "RRCA", 0, 1, func(cpu *CPU, operands []byte) { cpu.rrca() }},
+            }
+
+            //  ------------ 0X1N ----------------
+            0x10 => todo!(), // 0x10, "STOP", 1, 0, func(cpu *CPU, operands []byte) { cpu.stop() }},
+            0x11 => todo!(),
+            0x12 => todo!(),
+            0x13 => todo!(),
+            0x14 => todo!(),
+            0x15 => todo!(),
+            0x16 => todo!(),
+            0x17 => todo!(),
+            0x18 => todo!(),
+            0x19 => todo!(),
+            0x1A => todo!(),
+            0x1B => todo!(),
+            0x1C => todo!(),
+            0x1D => todo!(),
+            0x1E => todo!(),
+            0x1F => todo!(),
+
+            //  ------------ 0X2N ----------------
+            0x20 => todo!(),
+            0x21 => todo!(),
+            0x22 => todo!(),
+            0x23 => todo!(),
+            0x24 => todo!(),
+            0x25 => todo!(),
+            0x26 => todo!(),
+            0x27 => todo!(),
+            0x28 => todo!(),
+            0x29 => todo!(),
+            0x2A => todo!(),
+            0x2B => todo!(),
+            0x2C => todo!(),
+            0x2D => todo!(),
+            0x2E => todo!(),
+            0x2F => todo!(),
+
+            //  ------------ 0X3N ----------------
+            0x30 => todo!(),
+            0x31 => todo!(),
+            0x32 => todo!(),
+            0x33 => todo!(),
+            0x34 => todo!(),
+            0x35 => todo!(),
+            0x36 => todo!(),
+            0x37 => todo!(),
+            0x38 => todo!(),
+            0x39 => todo!(),
+            0x3A => todo!(),
+            0x3B => todo!(),
+            0x3C => todo!(),
+            0x3D => todo!(),
+            0x3E => todo!(),
+            0x3F => todo!(),
+
+            //  ------------ 0X4N ----------------
+            0x40 => todo!(),
+            0x41 => todo!(),
+            0x42 => todo!(),
+            0x43 => todo!(),
+            0x44 => todo!(),
+            0x45 => todo!(),
+            0x46 => todo!(),
+            0x47 => todo!(),
+            0x48 => todo!(),
+            0x49 => todo!(),
+            0x4A => todo!(),
+            0x4B => todo!(),
+            0x4C => todo!(),
+            0x4D => todo!(),
+            0x4E => todo!(),
+            0x4F => todo!(),
+
+            //  ------------ 0X5N ----------------
+            0x50 => todo!(),
+            0x51 => todo!(),
+            0x52 => todo!(),
+            0x53 => todo!(),
+            0x54 => todo!(),
+            0x55 => todo!(),
+            0x56 => todo!(),
+            0x57 => todo!(),
+            0x58 => todo!(),
+            0x59 => todo!(),
+            0x5A => todo!(),
+            0x5B => todo!(),
+            0x5C => todo!(),
+            0x5D => todo!(),
+            0x5E => todo!(),
+            0x5F => todo!(),
+
+            //  ------------ 0X6N ----------------
+            0x60 => todo!(),
+            0x61 => todo!(),
+            0x62 => todo!(),
+            0x63 => todo!(),
+            0x64 => todo!(),
+            0x65 => todo!(),
+            0x66 => todo!(),
+            0x67 => todo!(),
+            0x68 => todo!(),
+            0x69 => todo!(),
+            0x6A => todo!(),
+            0x6B => todo!(),
+            0x6C => todo!(),
+            0x6D => todo!(),
+            0x6E => todo!(),
+            0x6F => todo!(),
+
+            //  ------------ 0X7N ----------------
+            0x70 => todo!(),
+            0x71 => todo!(),
+            0x72 => todo!(),
+            0x73 => todo!(),
+            0x74 => todo!(),
+            0x75 => todo!(),
+            0x76 => todo!(),
+            0x77 => todo!(),
+            0x78 => todo!(),
+            0x79 => todo!(),
+            0x7A => todo!(),
+            0x7B => todo!(),
+            0x7C => todo!(),
+            0x7D => todo!(),
+            0x7E => todo!(),
+            0x7F => todo!(),
+
+            //  ------------ 0X8N ----------------
+            0x80 => todo!(),
+            0x81 => todo!(),
+            0x82 => todo!(),
+            0x83 => todo!(),
+            0x84 => todo!(),
+            0x85 => todo!(),
+            0x86 => todo!(),
+            0x87 => todo!(),
+            0x88 => todo!(),
+            0x89 => todo!(),
+            0x8A => todo!(),
+            0x8B => todo!(),
+            0x8C => todo!(),
+            0x8D => todo!(),
+            0x8E => todo!(),
+            0x8F => todo!(),
+
+            //  ------------ 0X9N ----------------
+            0x90 => todo!(),
+            0x91 => todo!(),
+            0x92 => todo!(),
+            0x93 => todo!(),
+            0x94 => todo!(),
+            0x95 => todo!(),
+            0x96 => todo!(),
+            0x97 => todo!(),
+            0x98 => todo!(),
+            0x99 => todo!(),
+            0x9A => todo!(),
+            0x9B => todo!(),
+            0x9C => todo!(),
+            0x9D => todo!(),
+            0x9E => todo!(),
+            0x9F => todo!(),
+
+            //  ------------ 0XAN ----------------
+            0xA0 => todo!(),
+            0xA1 => todo!(),
+            0xA2 => todo!(),
+            0xA3 => todo!(),
+            0xA4 => todo!(),
+            0xA5 => todo!(),
+            0xA6 => todo!(),
+            0xA7 => todo!(),
+            0xA8 => todo!(),
+            0xA9 => todo!(),
+            0xAA => todo!(),
+            0xAB => todo!(),
+            0xAC => todo!(),
+            0xAD => todo!(),
+            0xAE => todo!(),
+            0xAF => todo!(),
+
+            //  ------------ 0XBN ----------------
+            0xB0 => todo!(),
+            0xB1 => todo!(),
+            0xB2 => todo!(),
+            0xB3 => todo!(),
+            0xB4 => todo!(),
+            0xB5 => todo!(),
+            0xB6 => todo!(),
+            0xB7 => todo!(),
+            0xB8 => todo!(),
+            0xB9 => todo!(),
+            0xBA => todo!(),
+            0xBB => todo!(),
+            0xBC => todo!(),
+            0xBD => todo!(),
+            0xBE => todo!(),
+            0xBF => todo!(),
+
+            //  ------------ 0XCN ----------------
+            0xC0 => todo!(),
+            0xC1 => todo!(),
+            0xC2 => todo!(),
+            0xC3 => todo!(),
+            0xC4 => todo!(),
+            0xC5 => todo!(),
+            0xC6 => todo!(),
+            0xC7 => todo!(),
+            0xC8 => todo!(),
+            0xC9 => todo!(),
+            0xCA => todo!(),
+            0xCB => todo!(),
+            0xCC => todo!(),
+            0xCD => todo!(),
+            0xCE => todo!(),
+            0xCF => todo!(),
+
+            //  ------------ 0XDN ----------------
+            0xD0 => todo!(),
+            0xD1 => todo!(),
+            0xD2 => todo!(),
+            0xD3 => todo!(),
+            0xD4 => todo!(),
+            0xD5 => todo!(),
+            0xD6 => todo!(),
+            0xD7 => todo!(),
+            0xD8 => todo!(),
+            0xD9 => todo!(),
+            0xDA => todo!(),
+            0xDB => todo!(),
+            0xDC => todo!(),
+            0xDD => todo!(),
+            0xDE => todo!(),
+            0xDF => todo!(),
+
+            //  ------------ 0XEN ----------------
+            0xE0 => todo!(),
+            0xE1 => todo!(),
+            0xE2 => todo!(),
+            0xE3 => todo!(),
+            0xE4 => todo!(),
+            0xE5 => todo!(),
+            0xE6 => todo!(),
+            0xE7 => todo!(),
+            0xE8 => todo!(),
+            0xE9 => todo!(),
+            0xEA => todo!(),
+            0xEB => todo!(),
+            0xEC => todo!(),
+            0xED => todo!(),
+            0xEE => todo!(),
+            0xEF => todo!(),
+
+            //  ------------ 0XFN ----------------
+            0xF0 => todo!(),
+            0xF1 => todo!(),
+            0xF2 => todo!(),
+            0xF3 => todo!(),
+            0xF4 => todo!(),
+            0xF5 => todo!(),
+            0xF6 => todo!(),
+            0xF7 => todo!(),
+            0xF8 => todo!(),
+            0xF9 => todo!(),
+            0xFA => todo!(),
+            0xFB => todo!(),
+            0xFC => todo!(),
+            0xFD => todo!(),
+            0xFE => todo!(),
+            0xFF => todo!(),
+            // _ => bail!("not implemented opcode {:X}", opcode),
         }
 
         Ok(())
